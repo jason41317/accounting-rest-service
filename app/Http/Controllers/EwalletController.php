52271749e2dd0eb135cded5bb331f55d5e5bcf05
edprnl <pecsonerick.ep@gmail.com>
2021-02-26 16:15:22 +0800

    e-wallet and bank api
    e-wallet and bank api

--- app/Http/Controllers/EwalletController.php
+++ app/Http/Controllers/EwalletController.php
@@ -4,27 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Ewallet;
 use Illuminate\Http\Request;
+use App\Services\EWalletService;
+use App\Http\Resources\EWalletResource;
+use App\Http\Requests\EWalletStoreRequest;
+use App\Http\Requests\EWalletUpdateRequest;
 
 class EwalletController extends Controller
 {
-    /**
+ /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $eWalletService = new EWalletService();
+        $perPage = $request->per_page ?? 20;
+        $isPaginated = !$request->has('paginate') || $request->paginate === 'true';
+        $eWallets = $eWalletService->list($isPaginated, $perPage);
+        return EWalletResource::collection(
+            $eWallets
+        );
     }
 
     /**
@@ -33,53 +33,55 @@ class EwalletController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(EWalletStoreRequest $request)
     {
-        //
+        $eWalletService = new EWalletService();
+        $eWallet = $eWalletService->store($request->all());
+        return (new EWalletResource($eWallet))
+            ->response()
+            ->setStatusCode(201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Ewallet  $ewallet
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Ewallet $ewallet)
+    public function show(int $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Ewallet  $ewallet
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Ewallet $ewallet)
-    {
-        //
+        $eWalletService = new EWalletService();
+        $eWallet = $eWalletService->get($id);
+        return new EWalletResource($eWallet);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Ewallet  $ewallet
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Ewallet $ewallet)
+    public function update(EWalletUpdateRequest $request, int $id)
     {
-        //
+        $eWalletService = new EWalletService();
+        $eWallet = $eWalletService->update($request->all(), $id);
+
+        return (new EWalletResource($eWallet))
+            ->response()
+            ->setStatusCode(200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Ewallet  $ewallet
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Ewallet $ewallet)
+    public function destroy(int $id)
     {
-        //
+        $eWalletService = new EWalletService();
+        $eWalletService->delete($id);
+        return response()->json([], 204);
     }
 }
---
+++ app/Services/EWalletService.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\EWallet;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class EWalletService
+{
+  public function list(bool $isPaginated, int $perPage)
+  {
+    try {
+      $eWallets = $isPaginated
+        ? EWallet::paginate($perPage)
+        : EWallet::all();
+      return $eWallets;
+    } catch (Exception $e) {
+      Log::info('Error occured during EWalletService list method call: ');
+      Log::info($e->getMessage());
+      throw $e;
+    }
+  }
+
+  public function store(array $data)
+  {
+    DB::beginTransaction();
+    try {
+      $eWallet = EWallet::create($data);
+      DB::commit();
+      return $eWallet;
+    } catch (Exception $e) {
+      DB::rollback();
+      Log::info('Error occured during EWalletService store method call: ');
+      Log::info($e->getMessage());
+      throw $e;
+    }
+  }
+
+  public function get(int $id)
+  {
+    try {
+      $eWallet = EWallet::find($id);
+      return $eWallet;
+    } catch (Exception $e) {
+      Log::info('Error occured during EWalletService get method call: ');
+      Log::info($e->getMessage());
+      throw $e;
+    }
+  }
+
+  public function update(array $data, int $id)
+  {
+    DB::beginTransaction();
+    try {
+      $eWallet = EWallet::find($id);
+      $eWallet->update($data);
+      DB::commit();
+      return $eWallet;
+    } catch (Exception $e) {
+      DB::rollback();
+      Log::info('Error occured during EWalletService update method call: ');
+      Log::info($e->getMessage());
+      throw $e;
+    }
+  }
+
+  public function delete(int $id)
+  {
+    DB::beginTransaction();
+    try {
+      $eWallet = EWallet::find($id);
+      $eWallet->delete();
+      DB::commit();
+    } catch (Exception $e) {
+      DB::rollback();
+      Log::info('Error occured during EWalletService delete method call: ');
+      Log::info($e->getMessage());
+      throw $e;
+    }
+  }
+}
