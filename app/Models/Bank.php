d4576f2bf808b8a81e538647355d27926895e205
Jason Patawaran <patawaranjason17@gmail.com>
2021-03-22 09:53:38 +0800

    bank and ewallet permissions

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use App\Traits\SecureDelete;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bank extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, SecureDelete;
+    protected $guarded = ['id'];
+    protected $hidden = [
+        'created_at',
+        'deleted_at',
+        'updated_at',
+        'created_by',
+        'updated_by',
+        'deleted_by'
+    ];
 
     public function disbursements()
     {
