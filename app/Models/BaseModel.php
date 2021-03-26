6cf7e817a5a0517214a3ab939218d0b97543d566
edprnl <pecsonerick.ep@gmail.com>
2021-03-26 16:12:02 +0800

    apply trait on base model
    apply trait on base model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\SecureDelete;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
+
 class BaseModel extends Model implements Auditable
 {
-    use HasFactory, SoftDeletes, \OwenIt\Auditing\Auditable;
+    use HasFactory, SoftDeletes, SecureDelete, \OwenIt\Auditing\Auditable;
 }
